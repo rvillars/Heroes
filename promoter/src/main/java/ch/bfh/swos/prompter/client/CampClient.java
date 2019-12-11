eb4bcebfd98cb8d13fdd18c51db6ed26ede62bf0
Roger Villars <roger.villars@flowable.com>
2019-12-11 08:00:57 +0100

    - Upgrade to Spring Boot 2.2.2 - Upgrade to Spring Cloud Huxton - Moved maven properties to parent pom - Moved dependency management to parent pom

--- promoter/src/main/java/ch/bfh/swos/prompter/client/CampClient.java
+++ promoter/src/main/java/ch/bfh/swos/prompter/client/CampClient.java
@@ -2,7 +2,7 @@ package ch.bfh.swos.prompter.client;
 
 import ch.bfh.swos.prompter.model.Party;
 import org.springframework.cloud.openfeign.FeignClient;
-import org.springframework.hateoas.Resource;
+import org.springframework.hateoas.EntityModel;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
@@ -10,6 +10,6 @@ import org.springframework.web.bind.annotation.RequestParam;
 public interface CampClient {
 
     @GetMapping("/createParty")
-    Resource<Party> createParty(@RequestParam(value = "name") String name);
+    EntityModel<Party> createParty(@RequestParam(value = "name") String name);
 
 }
