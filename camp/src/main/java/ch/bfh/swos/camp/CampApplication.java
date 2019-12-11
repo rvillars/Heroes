3fdfa7cbbaac72ee98ebe627aa66632659017d44
Roger Villars <roger.villars@flowable.com>
2019-12-11 08:38:55 +0100

    - Fixed Hero model HP attribute to be double - Spring Boot 2.2.2

--- camp/src/main/java/ch/bfh/swos/camp/CampApplication.java
+++ camp/src/main/java/ch/bfh/swos/camp/CampApplication.java
@@ -2,7 +2,6 @@ package ch.bfh.swos.camp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.hateoas.config.EnableHypermediaSupport;
 
 @SpringBootApplication
 public class CampApplication {
